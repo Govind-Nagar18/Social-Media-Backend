e17aeb57146c37b433ee1e199482806afaaa43bf
Govind-Nagar18 <nagargovind2004@gmail.com>
2025-07-04 17:47:10 +0530

    url file update

--- Backend/urls.py
+++ Backend/urls.py
@@ -3,12 +3,20 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
-
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('', lambda request: JsonResponse({'message': 'Welcome to the Social Media API!'})),
     path('post/', include('myapp.urls')),
     path('register/', include('Custom_auth.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
 
