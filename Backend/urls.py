4a5314fb17a02090db8c9148d8f820460d9608e5
Govind-Nagar18 <nagargovind2004@gmail.com>
2025-07-04 15:33:55 +0530

    Add welcome message at root URL

--- Backend/urls.py
+++ Backend/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({'message': 'Welcome to the Social Media API!'})),
     path('post/', include('myapp.urls')),
     path('register/', include('Custom_auth.urls')),
     path('admin/', admin.site.urls),
